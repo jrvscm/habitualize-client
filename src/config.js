6652d118360de005f885f1c1481bc420f2df569d
Chris Jarvis <jrvscm@gmail.com>
2018-01-15 19:38:24 -0700

    troubleshooting ios issue

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 //https://pedantic-gates-f2f0cd.netlify.com
 
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL ||' https://habitualize-api.herokuapp.com/api';
+    process.env.REACT_APP_API_BASE_URL ||'https://habitualize-api.herokuapp.com/api';
