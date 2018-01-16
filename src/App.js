f47d05c5eed44e6e814ab3055325339b7b4cb9a8
Chris Jarvis <jrvscm@gmail.com>
2018-01-15 21:53:27 -0700

    add redirect from / to /home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
     Route,
+    Redirect,
     Switch
 } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
@@ -16,6 +17,7 @@ class App extends Component {
         <div className="pages-container">
           <main>
             <Switch>
+              <Redirect exact path from="/" to="/home" />
               <Route exact path="/" component={LandingPage} />
               <Route exact path="/login" component={LoginPage} /> 
               <Route exact path="/dashboard" component={UserDashboard} />
