8853d2e58159b99331916460c13c55e254fff3be
Chris Jarvis <jrvscm@gmail.com>
2018-01-15 20:00:35 -0700

    remove redirect to home on request

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
     Route,
-    Redirect,
     Switch
 } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
@@ -17,8 +16,7 @@ class App extends Component {
         <div className="pages-container">
           <main>
             <Switch>
-              <Redirect exact path from="/" to="/home" />
-              <Route exact path="/home" component={LandingPage} />
+              <Route exact path="/" component={LandingPage} />
               <Route exact path="/login" component={LoginPage} /> 
               <Route exact path="/dashboard" component={UserDashboard} />
               <Route exact path="/stats" component={HabitStats} />
